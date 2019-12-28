9bc66f8e6f73dab9eced622f39ff7eca0b664fe8
hussn <hussn@LAPTOP-BSNVQDBH>
2019-12-28 23:09:30 +0000

    Added new code

--- Practice/main.cpp
+++ Practice/main.cpp
@@ -4,6 +4,37 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
-	return 0;
+	//staircase problem
+	/*int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int s = 0; s < n - i; s++)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j < i; j++)
+		{
+			cout << "#";
+		}
+		cout << endl;
+	}*/
+
+	int arr[5] = {7, 69, 2, 221, 8974};
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = i + 1; j < 5; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				//swap(arr[j], arr[j + 1]);
+				int temp = 0;
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		cout << arr[i];
+	}
 }
