3e03f68608dc55ca07dee4df9e176a36e15f3e30
hussn <hussn@LAPTOP-BSNVQDBH>
2019-12-29 21:04:55 +0000

    Added new code

--- Practice/main.cpp
+++ Practice/main.cpp
@@ -22,19 +22,37 @@ int main()
 
 	int arr[5] = {7, 69, 2, 221, 8974};
 
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = i + 1; j < 5; j++)
-		{
-			if (arr[j] < arr[i])
-			{
-				//swap(arr[j], arr[j + 1]);
-				int temp = 0;
-				temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			}
+	//for (int i = 0; i < 5; i++)
+	//{
+	//	for (int j = i + 1; j < 5; j++)
+	//	{
+	//		if (arr[j] < arr[i])
+	//		{
+	//			//swap(arr[j], arr[j + 1]);
+	//			int temp = 0;
+	//			temp = arr[i];
+	//			arr[i] = arr[j];
+	//			arr[j] = temp;
+	//		}
+	//	}
+	//	cout << arr[i];
+	//}
+
+	long long min64bit = LLONG_MAX;
+	long long max64bit = 0;
+	long long temp64bit = 0;
+
+	for (int i = 0; i < 5; i++) {
+		temp64bit = 0;
+		for (int j = 0; j < 5; j++) {
+			if (i == j)
+				continue;
+			temp64bit += arr[j];
 		}
-		cout << arr[i];
+		if (temp64bit > max64bit)
+			max64bit = temp64bit;
+		if (temp64bit < min64bit)
+			min64bit = temp64bit;
 	}
+	cout << min64bit << " " << max64bit;
 }
